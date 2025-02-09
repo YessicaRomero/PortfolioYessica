439e094dddfb3e5b620888a6a96bb8a4a3f9579a
Yessica <yessilpd91@outlook.com>
2025-02-09 11:46:44 -0300

    agregando nueva ruta

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/PortfolioYessica/",
-  build: {
-    rollupOptions: {
-      input: './index.html',
-    },
-  },
+  base: "https://YessicaRomero.github.io/PortfolioYessica/",
+
 });
